b616a3973311372b57f6f6e196464a1298e3b0f0
munimuni5”git config --global user.name munimuni5” <emileeleamons@gmail.com>
2017-12-25 13:39:27 -0800

    up to sequelize db setup

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var app = express();
+
+//Set and Use statements to set up middleware
+// app.set('view engine', 'ejs');
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(ejsLayouts);
+// app.use(express.static(__dirname + '/public/'));
+//
+// //Controllers
+// app.use('/articles', require('./controllers/articles'));
+
+//Routes
+app.get('/', function(req, res) {
+  res.render('indexh.ejs');
+});
+// app.get('/', function(req,res){
+//   res.render('home');
+// });
+
+//Listen on port 3000
+app.listen(3000)
